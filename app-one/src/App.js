a7cd1a594948be02efc23bb692bedd73bda6465a
Jan Behrens <dev@jpbehrens.de>
2021-10-08 22:23:21 +0200

    update counter

--- app-one/src/App.js
+++ app-one/src/App.js
@@ -1,64 +1,43 @@
-import './App.css';
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 //import Greets from './Greets';
 import Counter from './Counter';
 
 
 function App() {
-  // USE STAE - React Hook
-  // [state, setState] = useState(initialState)
-  const [counter, setCounter] = useState(8);
 
-  // Settings des Counters
-  const counterSettings = {
-    steps: 2,
-    min: 2,
-    max: 2
+  const [counterList, setcounterList] = useState([
+    { id: 1, name: 'Counter A', count: 1 },
+    { id: 2, name: 'Counter B', count: 2 },
+    { id: 3, name: 'Counter C', count: 4 },
+    { id: 4, name: 'Counter D', count: 8 }
+  ]);
+
+
+  const handelCounterChange = (counter) => {
+    setcounterList(prevCounterList => prevCounterList.map(c => {
+      console.log('Counter:', counter);
+      console.log('C:', c);
+      if (c.id === counter.id) return counter;
+      return c;
+    }))
   }
 
-  // Funtion um den Couter hoch zu zählen 
-  const incremetCounter = () => {
-    setCounter(prevCounter =>
-      prevCounter < counterSettings.max ?
-        prevCounter += counterSettings.steps : prevCounter
-    )
-  };
-  // Funtion um den Couter runter zu zählen 
-  const decremetCounter = () => {
-    setCounter(prevCounter =>
-      prevCounter > counterSettings.min ?
-        prevCounter -= counterSettings.steps : prevCounter
-    )
-  };
-
-  // USE Effect - React Hook
-  useEffect(() => {
-    //effect
-    const interval = setInterval(() => {
-      incremetCounter();
-    }, 23500)
-    return () => {
-      //cleanup wenn man die die funtion nicht mehr nutzt kommt der Cleanup der dann den effevct unmounted
-      clearInterval(interval)
-    }
-  }, []);
+  const cointerList = counterList.map(counter => (
+    <Counter
+      key={counter.id}
+      name={counter.name}
+      count={counter.count}
+      id={counter.id}
+      onChangeCounter={handelCounterChange}
+    />
+  ));
 
   return (
     <>
-      <Counter
-        counter={counter}
-        incremetCounter={incremetCounter}
-        decremetCounter={decremetCounter}
-        settings={counterSettings}
-      />
-      <Counter
-        counter={counter}
-        incremetCounter={incremetCounter}
-        decremetCounter={decremetCounter}
-        settings={counterSettings}
-      />
+      <h1>Counter Liste</h1>
+      {cointerList}
     </>
-  );
+  )
 }
 
 export default App;
