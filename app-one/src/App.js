e317e5ec179eba8d5b4870d4ca7f4bd4f88ee2b0
Jan Behrens <dev@jpbehrens.de>
2021-10-10 13:25:12 +0200

    add component todo

--- app-one/src/App.js
+++ app-one/src/App.js
@@ -1,14 +1,65 @@
-import React from 'react'
-import './index.css';
-import Greets from './componentes/Greets'
-import CounterList from './componentes/Couter/CounterList'
+import React, { useState } from 'react'
+import './todo-list.css';
 
 const App = () => {
-    return (
-        <>
-          <Greets name="Max"/>
-          <CounterList/> 
-        </>
-    )
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Testaufgabe',
+      done: false
+    },
+    {
+      id: 2,
+      title: 'Testaufgabe 2',
+      done: false
+    }
+
+  ]);
+
+  // Funtion um ToDo hinzu zu fügen
+const handleAddTodo = (e) => {
+  e.preventDefault();
+  alert('test');
+
+}
+
+
+  return (
+    <>
+      <header className="todo-header">
+        <h1 className="todo-brandname">
+          Todo Liste
+        </h1>
+        <form className="todo-search">
+          <input onKeyDown={handleAddTodo} type="search" placeholder="Suche..." />
+        </form>
+      </header>
+
+      <form className="add-todo">
+        <input type="text" name="new-todo" /><button>add</button>
+      </form>
+
+      <main className="todo-main">
+
+        <h2>Offene Aufgaben</h2>
+
+        {todos.map(todo => (
+          <div className="todo-item">
+            <input type="checkbox" name="" />
+            <span>{todo.title}</span>
+            <button>löschen</button>
+          </div>
+
+        ))}
+
+
+
+
+        <h2>Erledigte Aufgaben</h2>
+
+      </main>
+    </>
+  )
 }
 export default App
