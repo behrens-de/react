83c56fff625e7ea9adb302580cd3531043823943
Jan Behrens <dev@jpbehrens.de>
2021-10-13 00:06:14 +0200

    update shoppinglist

--- shoppinglist/src/App.js
+++ shoppinglist/src/App.js
@@ -6,7 +6,8 @@ function App() {
   const handleAddList = () => {
     const id = myList.length+1;
     const input = document.getElementById("add-list").value;
-    setMyList(myList => [...myList, {id,name:input}]);
+    // Prüffen ob nicht Leer oder schon vorhanden 
+    setMyList(myList => [...myList, {id,name:input.trim()}]);
     document.getElementById("add-list").value = '';
   }
 
@@ -18,13 +19,26 @@ function App() {
 
   const showMyList = myList.map(({name,id})=>(
     <>
-    <option key={id}>{id}({name}</option>
+    <option key={id}>{name}</option>
     </>
     ))
+
+
+  const showListQuanty = () =>{
+    const quanty = myList.length;
+    let req;
+    if(quanty < 1){ req = "Keine Einträge"}
+    if(quanty === 1){ req =  "Ein Eintrag"}
+    if(quanty > 1){ req = quanty+" Einträge"}
+    return req;
+  }  
+
+  const allLists = showListQuanty();
 
   return (
     <div className="App">
       <div>
+        <h1>{allLists}</h1>
       <select>{showMyList}</select>
       </div>
       <input onKeyDown={handelInputChange} type="text" id="add-list" />
